08c59d78ee3d63c1d3545ddeb75a36938f9afc22
Alfred Santos <alfie.g.santos@gmail.com>
2019-07-31 20:22:02 -0500

    bio

--- js/app.js
+++ js/app.js
@@ -113,192 +113,192 @@ google.maps.Map.prototype.setCenterWithOffset= function(latlng, offsetX, offsetY
  * This function initialize google map. More info on 
  * https://developers.google.com/maps/documentation/javascript/
  */
- function initializeMap() {
+//  function initializeMap() {
 
- 	var mapOptions = {
- 		zoom: 12,
- 		zoomControl: false,
- 		scaleControl: false,
- 		scrollwheel: false,
- 		draggable: false,
- 		center: myLangLat,
- 		mapTypeControlOptions: {
- 			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
- 		},
- 		styles: [
- 		{
- 			"featureType": "administrative.locality",
- 			"elementType": "all",
- 			"stylers": [
- 			{
- 				"hue": "#2c2e33"
- 			},
- 			{
- 				"saturation": 7
- 			},
- 			{
- 				"lightness": 19
- 			},
- 			{
- 				"visibility": "on"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "landscape",
- 			"elementType": "all",
- 			"stylers": [
- 			{
- 				"hue": "#ffffff"
- 			},
- 			{
- 				"saturation": -100
- 			},
- 			{
- 				"lightness": 100
- 			},
- 			{
- 				"visibility": "simplified"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "poi",
- 			"elementType": "all",
- 			"stylers": [
- 			{
- 				"hue": "#ffffff"
- 			},
- 			{
- 				"saturation": -100
- 			},
- 			{
- 				"lightness": 100
- 			},
- 			{
- 				"visibility": "off"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "road",
- 			"elementType": "geometry",
- 			"stylers": [
- 			{
- 				"hue": "#bbc0c4"
- 			},
- 			{
- 				"saturation": -93
- 			},
- 			{
- 				"lightness": 31
- 			},
- 			{
- 				"visibility": "simplified"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "road",
- 			"elementType": "labels",
- 			"stylers": [
- 			{
- 				"hue": "#bbc0c4"
- 			},
- 			{
- 				"saturation": -93
- 			},
- 			{
- 				"lightness": 31
- 			},
- 			{
- 				"visibility": "on"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "road.arterial",
- 			"elementType": "labels",
- 			"stylers": [
- 			{
- 				"hue": "#bbc0c4"
- 			},
- 			{
- 				"saturation": -93
- 			},
- 			{
- 				"lightness": -2
- 			},
- 			{
- 				"visibility": "simplified"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "road.local",
- 			"elementType": "geometry",
- 			"stylers": [
- 			{
- 				"hue": "#e9ebed"
- 			},
- 			{
- 				"saturation": -90
- 			},
- 			{
- 				"lightness": -8
- 			},
- 			{
- 				"visibility": "simplified"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "transit",
- 			"elementType": "all",
- 			"stylers": [
- 			{
- 				"hue": "#e9ebed"
- 			},
- 			{
- 				"saturation": 10
- 			},
- 			{
- 				"lightness": 69
- 			},
- 			{
- 				"visibility": "on"
- 			}
- 			]
- 		},
- 		{
- 			"featureType": "water",
- 			"elementType": "all",
- 			"stylers": [
- 			{
- 				"hue": "#e9ebed"
- 			},
- 			{
- 				"saturation": -78
- 			},
- 			{
- 				"lightness": 67
- 			},
- 			{
- 				"visibility": "simplified"
- 			}
- 			]
- 		}
- 		]
- 	};
+//  	var mapOptions = {
+//  		zoom: 12,
+//  		zoomControl: false,
+//  		scaleControl: false,
+//  		scrollwheel: false,
+//  		draggable: false,
+//  		center: myLangLat,
+//  		mapTypeControlOptions: {
+//  			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+//  		},
+//  		styles: [
+//  		{
+//  			"featureType": "administrative.locality",
+//  			"elementType": "all",
+//  			"stylers": [
+//  			{
+//  				"hue": "#2c2e33"
+//  			},
+//  			{
+//  				"saturation": 7
+//  			},
+//  			{
+//  				"lightness": 19
+//  			},
+//  			{
+//  				"visibility": "on"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "landscape",
+//  			"elementType": "all",
+//  			"stylers": [
+//  			{
+//  				"hue": "#ffffff"
+//  			},
+//  			{
+//  				"saturation": -100
+//  			},
+//  			{
+//  				"lightness": 100
+//  			},
+//  			{
+//  				"visibility": "simplified"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "poi",
+//  			"elementType": "all",
+//  			"stylers": [
+//  			{
+//  				"hue": "#ffffff"
+//  			},
+//  			{
+//  				"saturation": -100
+//  			},
+//  			{
+//  				"lightness": 100
+//  			},
+//  			{
+//  				"visibility": "off"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "road",
+//  			"elementType": "geometry",
+//  			"stylers": [
+//  			{
+//  				"hue": "#bbc0c4"
+//  			},
+//  			{
+//  				"saturation": -93
+//  			},
+//  			{
+//  				"lightness": 31
+//  			},
+//  			{
+//  				"visibility": "simplified"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "road",
+//  			"elementType": "labels",
+//  			"stylers": [
+//  			{
+//  				"hue": "#bbc0c4"
+//  			},
+//  			{
+//  				"saturation": -93
+//  			},
+//  			{
+//  				"lightness": 31
+//  			},
+//  			{
+//  				"visibility": "on"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "road.arterial",
+//  			"elementType": "labels",
+//  			"stylers": [
+//  			{
+//  				"hue": "#bbc0c4"
+//  			},
+//  			{
+//  				"saturation": -93
+//  			},
+//  			{
+//  				"lightness": -2
+//  			},
+//  			{
+//  				"visibility": "simplified"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "road.local",
+//  			"elementType": "geometry",
+//  			"stylers": [
+//  			{
+//  				"hue": "#e9ebed"
+//  			},
+//  			{
+//  				"saturation": -90
+//  			},
+//  			{
+//  				"lightness": -8
+//  			},
+//  			{
+//  				"visibility": "simplified"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "transit",
+//  			"elementType": "all",
+//  			"stylers": [
+//  			{
+//  				"hue": "#e9ebed"
+//  			},
+//  			{
+//  				"saturation": 10
+//  			},
+//  			{
+//  				"lightness": 69
+//  			},
+//  			{
+//  				"visibility": "on"
+//  			}
+//  			]
+//  		},
+//  		{
+//  			"featureType": "water",
+//  			"elementType": "all",
+//  			"stylers": [
+//  			{
+//  				"hue": "#e9ebed"
+//  			},
+//  			{
+//  				"saturation": -78
+//  			},
+//  			{
+//  				"lightness": 67
+//  			},
+//  			{
+//  				"visibility": "simplified"
+//  			}
+//  			]
+//  		}
+//  		]
+//  	};
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    map.setCenterWithOffset(myLangLat, 0, -55);
+//     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+//     map.setCenterWithOffset(myLangLat, 0, -55);
 
-}
-google.maps.event.addDomListener(window, 'load', initializeMap);
+// }
+// google.maps.event.addDomListener(window, 'load', initializeMap);
 
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenterWithOffset(myLangLat, 0, -55);
-});
+// google.maps.event.addDomListener(window, 'resize', function() {
+//     map.setCenterWithOffset(myLangLat, 0, -55);
+// });
 
 
